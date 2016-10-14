901dbff607177a68a11c6af4ab5812cb49003687
Jason Tamez <jasontamez@users.noreply.github.com>
2016-10-14 02:01:54 -0500

    Now use a global variable! (plus extra alias <extag>)

--- ExtraTag.hooks.php
+++ ExtraTag.hooks.php
@@ -14,13 +14,23 @@ class ExtraTagHooks {
 	public static function onParserFirstCallInit( Parser $parser ) {
 		// When the parser sees the <sample> tag, it executes renderTagSample (see below)
 		$parser->setHook( 'extratag', array(__CLASS__, 'renderTagExtraTag') );
+		$parser->setHook( 'extag', array(__CLASS__, 'renderTagExtraTag') );
 		$parser->setHook( 'etag', array(__CLASS__, 'renderTagExtraTag') );
 	}
 
 	// Render <etag>
 	public static function renderTagExtraTag( $input, array $args, Parser $parser, PPFrame $frame ) {
 		// Nothing exciting here, just escape the user-provided input and throw it back out again (as example)
-		return "*Test\n*Complete\n\n".htmlspecialchars( $input );
+		global $wgExtraTagSubstitutions;
+		$pre = '';
+		$post = '';
+		foreach ($args as $tag => array $code) {
+		  if(isset($wgExtraTagSubstitutions[$tag]) {
+		    $pre = array_shift($code);
+		    $post = array_shift($code);
+		  }
+		}
+		return $pre.htmlspecialchars( $input ).$post;
 	}
 }
 
