79ebdd05ba6347e4e1804bd4557e0f0a80ab4064
Jason Tamez <jasontamez@users.noreply.github.com>
2016-10-14 01:38:24 -0500

    Public static functions; test output for the tag; fixed class

--- ExtraTag.hooks.php
+++ ExtraTag.hooks.php
@@ -11,16 +11,16 @@
 
 class ExtraTagHooks {
 	// Register any render callbacks with the parser
-	function onParserFirstCallInit( Parser $parser ) {
+	public static function onParserFirstCallInit( Parser $parser ) {
 		// When the parser sees the <sample> tag, it executes renderTagSample (see below)
-		$parser->setHook( 'extratag', 'ExtraTag::renderTagExtraTag' );
-		$parser->setHook( 'etag', 'ExtraTag::renderTagExtraTag' );
+		$parser->setHook( 'extratag', array(__CLASS__, 'renderTagExtraTag') );
+		$parser->setHook( 'etag', array(__CLASS__, 'renderTagExtraTag') );
 	}
 
 	// Render <etag>
-	function renderTagExtraTag( $input, array $args, Parser $parser, PPFrame $frame ) {
+	public static function renderTagExtraTag( $input, array $args, Parser $parser, PPFrame $frame ) {
 		// Nothing exciting here, just escape the user-provided input and throw it back out again (as example)
-		return htmlspecialchars( $input );
+		return "*Test\n*Complete\n\n".htmlspecialchars( $input );
 	}
 }
 
