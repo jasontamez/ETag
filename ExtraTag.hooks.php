ea2fe720f48e732be4a311dc90d0ba19708367a5
Jason Tamez <jasontamez@users.noreply.github.com>
2016-10-16 17:23:41 -0500

    Name change + CSS support
    Removed the abbreviated tags, as well. For some reason, it was blocking
    the CSS injection when you used more than one tag name.

--- ExtraTag.hooks.php
+++ ExtraTag.hooks.php
@@ -1,25 +1,20 @@
 <?php
 /**
- * Hooks for ExtraTag extension
+ * Hooks for LocalTag extension
  *
  * @file
  * @ingroup Extensions
  */
 
-
-class ExtraTagHooks {
+class LocalTagHooks {
 	// Register any render callbacks with the parser
 	public static function onParserFirstCallInit( Parser $parser ) {
-		// When the parser sees the <sample> tag, it executes renderTagSample (see below)
-		$parser->setHook( 'extratag', array(__CLASS__, 'renderTagExtraTag') );
-		$parser->setHook( 'extag', array(__CLASS__, 'renderTagExtraTag') );
-		$parser->setHook( 'etag', array(__CLASS__, 'renderTagExtraTag') );
+		$parser->setHook( 'localtag', array(__CLASS__, 'renderTagLocalTag') );
 	}
 
-	// Render <etag>
-	public static function renderTagExtraTag( $input, array $args, Parser $parser, PPFrame $frame ) {
-		// Nothing exciting here, just escape the user-provided input and throw it back out again (as example)
-		global $wgExtraTagSubstitutions;
+	// Render <localtag>
+	public static function renderTagLocalTag( $input, array $args, Parser $parser, PPFrame $frame ) {
+		global $wgLocalTagSubstitutions;
 		$pre = '';
 		$post = '';
 		foreach ($args as $name => $value) {
@@ -27,14 +22,20 @@ class ExtraTagHooks {
 		  // Currently, this will only use the LAST found argument, if multiple are given.
 		  // To-do: - Is there any way to utilize any/all arguments?
 		  //        - Is there any reason to pass the arguments' values?
-		  if(isset($wgExtraTagSubstitutions[$name])) {
-		    // 'argument' => array('pre text', 'post text');
-		    $code = $wgExtraTagSubstitutions[$name];
-		    $pre = array_shift($code);
-		    $post = array_shift($code);
+		  if(isset($wgLocalTagSubstitutions[$name])) {
+		  	// 'argument' => array('pre text', 'post text', 'css');
+		  	$code = $wgLocalTagSubstitutions[$name];
+		  	$pre = array_shift($code);
+		  	$post = array_shift($code);
+		  	$css = array_shift($code);
+		  	if($css) {
+		  		$parser->getOutput()->addHeadItem('<style type="text/css">'.$css.'</style>');
+			  	// Remove CSS from global, thus preventing multiple injections.
+			  	$wgLocalTagSubstitutions[$name] = array($pre, $post, '');
+		  	}
 		  }
 		}
-		// Parse wikitext inside <extratag></extratag>
+		// Parse wikitext inside <localtag></localtag>
 		$output = $parser->recursiveTagParse( $input, $frame );
 		return $pre.$output.$post;
 	}
