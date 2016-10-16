1e7cd6979477319a194e87498195b14535f47fa4
Jason Tamez <jasontamez@users.noreply.github.com>
2016-10-16 17:20:42 -0500

    Locking down final (I hope) name change.

--- ExtraTag.php
+++ ExtraTag.php
@@ -1,15 +1,15 @@
 <?php
 
 if ( function_exists( 'wfLoadExtension' ) ) {
-	wfLoadExtension( 'ExtraTag' );
+	wfLoadExtension( 'LocalTag' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
-	$wgMessagesDirs['ExtraTag'] = __DIR__ . '/i18n';
-	$wgExtensionMessagesFiles['ExtraTagAlias'] = __DIR__ . '/ETag.i18n.alias.php';
+	$wgMessagesDirs['LocalTag'] = __DIR__ . '/i18n';
+	$wgExtensionMessagesFiles['LocalTagAlias'] = __DIR__ . '/LocalTag.i18n.alias.php';
 	wfWarn(
-		'Deprecated PHP entry point used for ExtraTag extension. Please use wfLoadExtension ' .
+		'Deprecated PHP entry point used for LocalTag extension. Please use wfLoadExtension ' .
 		'instead, see https://www.mediawiki.org/wiki/Extension_registration for more details.'
 	);
 	return true;
 } else {
-	die( 'This version of the ExtraTag extension requires MediaWiki 1.25+' );
+	die( 'This version of the LocalTag extension requires MediaWiki 1.25+' );
 }
