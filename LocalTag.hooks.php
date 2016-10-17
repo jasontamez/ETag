9345890c5ca0df2bd6f84d7b5510885b03694b04
Jason Tamez <jasontamez@users.noreply.github.com>
2016-10-16 21:30:58 -0500

    Comments, comments, and multiple args.
    CSS now prints with comments denoting what triggered it.
    
    Code now has lots more comments.
    
    Now handles multiple arguments inside a single tag.

--- LocalTag.hooks.php
+++ LocalTag.hooks.php
@@ -14,28 +14,47 @@ class LocalTagHooks {
 
 	// Render <localtag>
 	public static function renderTagLocalTag( $input, array $args, Parser $parser, PPFrame $frame ) {
+		// Load global with the user-defined definitions.
 		global $wgLocalTagSubstitutions;
+		// Variable for text going before the content.
 		$pre = '';
+		// Variable for text going after the content.
 		$post = '';
+		// Variable holding any/all CSS needed.
+		$css = array();
+		// Variable holding all valid, defined arguments referenced in the localtag.
+		$subs = array();
 		foreach ($args as $name => $value) {
-		  // Check user-defined variables.
-		  // Currently, this will only use the LAST found argument, if multiple are given.
+		  // Check for user-defined definitions.
+		  // Multiple arguments are possible. They will be wrapped in order around the content.
+		  // Eg:  IN: <localtag foo bar baz>
+		  //     OUT: <foo><bar><baz>content</baz></bar></foo>
 		  // To-do: - Is there any way to utilize any/all arguments?
 		  //        - Is there any reason to pass the arguments' values?
 		  if(isset($wgLocalTagSubstitutions[$name])) {
 		  	// 'argument' => array('pre text', 'post text', 'css');
 		  	$code = $wgLocalTagSubstitutions[$name];
-		  	$pre = array_shift($code);
-		  	$post = array_shift($code);
-		  	$css = array_shift($code);
-		  	if($css) {
-		  		$parser->getOutput()->addHeadItem('<style type="text/css">'.$css.'</style>');
+		  	$foo = array_shift($code); // pre
+		  	$bar = array_shift($code); // post
+		  	$baz = array_shift($code); // css
+		  	$pre .= $foo;
+		  	$post = $bar.$post;
+		  	$subs[] = $name;
+		  	if($baz) {
 			  	// Remove CSS from global, thus preventing multiple injections.
-			  	$wgLocalTagSubstitutions[$name] = array($pre, $post, '');
+			  	$wgLocalTagSubstitutions[$name] = array($foo, $bar, '');
+			  	// Store CSS
+			  	$css[] = $baz;
 		  	}
 		  }
 		}
-		// Parse wikitext inside <localtag></localtag>
+		if($css !== array()) {
+			// Send collected CSS with a comment denoting what it's for.
+			array_unshift($css, '<style type="text/css">', '/* CSS for LocalTag: '.implode(' ', $subs).' */');
+			$CSS = implode("\n\t", $css)."\n</style>\n";
+			$parser->getOutput()->addHeadItem($CSS);
+		}
+		// Parse content inside <localtag></localtag> as wikitext.
 		$output = $parser->recursiveTagParse( $input, $frame );
 		return $pre.$output.$post;
 	}
