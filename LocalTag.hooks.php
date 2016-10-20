be6b05f6aa2c852e0adb53c57e69782c54e03d2f
Jason Tamez <jasontamez@users.noreply.github.com>
2016-10-19 23:03:37 -0500

    more comments, slight code restructure

--- LocalTag.hooks.php
+++ LocalTag.hooks.php
@@ -40,8 +40,8 @@ class LocalTagHooks {
 		// INCORRECT VALUE HANDLING:
 		// 
 		// 	Wikitext:	<localtag example>
-		// 	Definition:	<li>@@</li>
-		// 	Output: 	<li></li>
+		// 	Definition:	<li>@@</li><li>@@</li>
+		// 	Output: 	<li></li><li></li>
 		// 	(No value == empty string)
 		// 
 		// 	Wikitext:	<localtag example="one!two">
@@ -65,90 +65,91 @@ class LocalTagHooks {
 		$css = [];
 		// Variable holding all valid, defined attributes referenced in the localtag.
 		$subs = [];
-		// HTML is case insensitive. So, make user-defined attributes lowercase.
-		$wgLocalTagSubstitutions = array_change_key_case( $wgLocalTagSubstitutions, CASE_LOWER );
-		$args = array_change_key_case( $args, CASE_LOWER );
-		foreach ( $args as $name => $value ) {
-			// Check for administrator-defined attributes.
-			// Multiple attributes are possible. They will be wrapped in order around the content.
-			// Eg:  IN: <localtag foo bar baz>
-			//     OUT: <foo><bar><baz>content</baz></bar></foo>
-			if ( isset( $wgLocalTagSubstitutions[$name] ) ) {
-				// 'attribute' => array( 'pre text', 'post text', 'css' );
-				$code = $wgLocalTagSubstitutions[$name];
-				$originalPRE = array_shift( $code ); // pre
-				$originalPOST = array_shift( $code ); // post
-				$originalCSS = array_shift( $code ); // css
-				$givens = explode( $sep, $value ); // user-given arguments
-				// Load the pre and post text, separated by $mark
-				// $limit is used to mark the start and end, and the break between pre/post
-				// (this catches a definition marker at the start or end of pre/post)
-				$limit = uniqid();
-				$needed = explode( $mark, $originalPRE );
-				array_unshift( $needed, $limit );
-				$needed[] = $limit;
-				$needed = array_merge( $needed, explode( $mark, $originalPOST ) );
-				$FLAG = 0;
-				while ( $needed ) {
-					$testcase = array_shift( $needed );
-					if ( $testcase === $limit) {
-						switch ($FLAG) {
-							case 0:
-								// Case 0 (initial), set to true (handle pre text)
-								$FLAG = true;
-								break;
-							case true:
-								// Case true, set to false (handle post text)
-								$FLAG = false;
-							// Case false unneeded.
-						}
-					} else {
-						if ( $FLAG === true ) {
-							// We're in pre text
-							$pre .= $testcase;
-						} else {
-							// We're in post text
-							$post .= $testcase;
-						}
-						if ( $needed && $needed[0] !== $limit ) {
-							// We need to insert an argument
-							if ( $givens ) {
-								// We have argument(s) for insertion
-								if ( $FLAG ) {
-									$pre .= array_shift( $givens );
-								} else {
-									$post .= array_shift( $givens );
-								}
-							}
-						} // endif
-					} // end if/else
-				} // end while
-				// Store this attribute in the list of substitutions used.
-				$subs[] = $name;
-				if ( $originalCSS ) {
-					// Remove CSS from global, thus preventing multiple injections.
-					$wgLocalTagSubstitutions[$name] = [ $originalPRE, $originalPOST, '' ];
-					// Store CSS (if not blank)
-					if( trim( $originalCSS ) ) {
-						$css[] = $originalCSS;
-					}
-				}
-			} elseif ( $verbose ) {
-				// Attribute not found. Alert via a message.
-				$pre .= "[&gt;localtag $name&lt; not found]";
+		if( $args === [] ) {
+			// No attributes were set! Nothing to do but throw an error, if desired.
+			if( $verbose ) {
+				$pre .= '[<strong>Error:</strong> &gt;localtag&lt; requires an/some attribute(s) to be effective]';
 			}
-		}
-		if ( $css !== [] ) {
-			// Send collected CSS with a comment denoting what it's for.
-			array_unshift( $css,
-					'<style type="text/css">',
-					'/* CSS for LocalTag: '.implode( ' ', $subs ).' */' );
-			$CSS = implode( "\n\t", $css )."\n</style>\n";
-			$parser->getOutput()->addHeadItem( $CSS );
-		}
+		} else {
+			// HTML is case insensitive. So, make user-defined attributes lowercase.
+			$wgLocalTagSubstitutions = array_change_key_case( $wgLocalTagSubstitutions, CASE_LOWER );
+			$args = array_change_key_case( $args, CASE_LOWER );
+			foreach ( $args as $name => $value ) {
+				// Check for administrator-defined attributes.
+				// Multiple attributes are possible. They will be wrapped in order around the content.
+				// Eg:  IN: <localtag foo bar baz>
+				//     OUT: <foo><bar><baz>content</baz></bar></foo>
+				if ( isset( $wgLocalTagSubstitutions[$name] ) ) {
+					// 'attribute' => array( 'pre text', 'post text', 'css' );
+					$code = $wgLocalTagSubstitutions[$name];
+					$originalPRE = array_shift( $code ); // pre
+					$originalPOST = array_shift( $code ); // post
+					$originalCSS = array_shift( $code ); // css
+					$givens = explode( $sep, $value ); // user-given arguments
+					// Load the pre and post text, separated by $mark
+					// Ignore '' pre/post text (since $pre and $post are already '')
+					// $limit is used to mark the break between pre/post
+					// (this catches a definition marker at the start or end of pre/post)
+					$limit = uniqid();
+					$needed = $originalPRE ? explode( $mark, $originalPRE ) : [];
+					$needed[] = $limit;
+					$needed = array_merge( $needed, ( $originalPOST ? explode( $mark, $originalPOST ) : [] ) );
+					$FLAG = true;
+					while ( $needed ) {
+						$testcase = array_shift( $needed );
+						if ( $testcase === $limit) {
+							// We're done with pre text, switch to post text
+							$FLAG = false;
+						} else {
+							if ( $FLAG === true ) {
+								// We're in pre text
+								$pre .= $testcase;
+							} else {
+								// We're in post text
+								$post .= $testcase;
+							}
+							if ( $needed && $needed[0] !== $limit ) {
+								// We're not at the end of the post text
+								// && We're not at the end of the pre text
+								// Therefore, we're at a break and need to insert an argument
+								if ( $givens ) {
+									// We have argument(s) for insertion
+									if ( $FLAG ) {
+										$pre .= array_shift( $givens );
+									} else {
+										$post .= array_shift( $givens );
+									}
+								}
+							} // endif
+						} // end if/else
+					} // end while
+					// Store this attribute in the list of substitutions used.
+					$subs[] = $name;
+					if ( $originalCSS ) {
+						// Remove CSS from global, thus preventing multiple injections.
+						$wgLocalTagSubstitutions[$name] = [ $originalPRE, $originalPOST, '' ];
+						// Store CSS (if not blank)
+						if( trim( $originalCSS ) ) {
+							$css[] = $originalCSS;
+						}
+					}
+				} elseif ( $verbose ) {
+					// Attribute not found. Alert via a message.
+					$pre .= "[<strong>Error:</strong> &gt;localtag $name&lt; not defined]";
+				} // end isset() if
+			} // end foreach loop
+			if ( $css !== [] ) {
+				// Send collected CSS with a comment denoting what it's for.
+				array_unshift( $css,
+						'<style type="text/css">',
+						'/* CSS for LocalTag: '.implode( ' ', $subs ).' */' );
+				$CSS = implode( "\n\t", $css )."\n</style>\n";
+				$parser->getOutput()->addHeadItem( $CSS );
+			} // end $css if
+		} // end $args if
 		// Parse content inside <localtag></localtag> as wikitext.
 		$output = $parser->recursiveTagParse( $input, $frame );
 		return $pre.$output.$post;
-	}
+	} // end render function
 }
 
